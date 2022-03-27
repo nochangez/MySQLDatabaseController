86e9542cf31fdeda43421af1bf3cb9bc0e9b4aba
nochangez <62546668+nochangez@users.noreply.github.com>
2022-03-27 13:06:29 +0300

    Add files via upload

---
+++ database_controller.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+
+from getpass import getpass
+
+from mysql.connector import connect, Error
+
+
+class DatabaseController:
+    def __init__(self):
+        # try to connect to database
+        try:
+            # try to connect to the database and init connection
+            self.connection = connect(
+                user=str(input("[db] enter your username: ")),
+                password=getpass("[db] enter your password: "),
+                database=str(input("[db] enter database name: ")),
+            )  # init db connection
+
+            self.cursor = self.connection.cursor()  # init db cursor
+
+            # make an indent
+            print("\n")
+        except Error as database_error:
+            print(f"[db] an error has occurred: {database_error}")
+
+    def get_data_execute_query(self, database_query):
+        self.cursor.execute(database_query)  # make a query
+
+        back_data = self.cursor.fetchall()  # got data
+
+        return back_data  # return got data
+
+    def execute_query(self, database_query):
+        self.cursor.execute(database_query)  # make a query
+        self.connection.commit()  # save changes
+
+        print("[db] query done")
